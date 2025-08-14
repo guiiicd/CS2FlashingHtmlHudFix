0a7c8ebb18b9d0a8760afad120cce4f839a94565
Deana <grcjn.b.rcki@gmail.com>
2025-08-14 20:04:02 +0100

    Update FlashingHtmlHudFix.cs

--- src/FlashingHtmlHudFix.cs
+++ src/FlashingHtmlHudFix.cs
@@ -8,40 +8,51 @@ namespace FlashingHtmlHudFix
     public partial class FlashingHtmlHudFix : BasePlugin
     {
         public override string ModuleName => "FlashingHtmlHudFix";
-        public override string ModuleVersion => "1.0";
-        public override string ModuleAuthor => "Deana https://x.com/dea_bb/";
+        public override string ModuleVersion => "1.1";
+        public override string ModuleAuthor => "Deana https://x.com/girlglock_/";
         public override string ModuleDescription => "A Plugin that fixes Html Hud.";
 
         private CCSGameRules? _gameRules;
+        private bool _gameRulesInitialized;
 
         public override void Load(bool hotReload)
         {
             RegisterListener<Listeners.OnTick>(OnTick);
             RegisterListener<Listeners.OnMapStart>(OnMapStartHandler);
+
+            if (hotReload)
+            {
+                InitializeGameRules();
+            }
         }
 
         private void OnMapStartHandler(string mapName)
         {
             _gameRules = null;
+            _gameRulesInitialized = false;
         }
 
         private void InitializeGameRules()
         {
+            if (_gameRulesInitialized) return;
+            
             var gameRulesProxy = Utilities.FindAllEntitiesByDesignerName<CCSGameRulesProxy>("cs_gamerules").FirstOrDefault();
             _gameRules = gameRulesProxy?.GameRules;
+            _gameRulesInitialized = _gameRules != null;
         }
 
         private void OnTick()
         {
-            if (_gameRules == null)
+            if (!_gameRulesInitialized)
             {
                 InitializeGameRules();
+                return;
             }
-            else
+
+            if (_gameRules != null)
             {
                 _gameRules.GameRestart = _gameRules.RestartRoundTime < Server.CurrentTime;
             }
         }
-        //thx for watching make sure to like and subscribe!
     }
 }
